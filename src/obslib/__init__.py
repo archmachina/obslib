84568d01d7c59e668b9f6f51d57a52d120b07739
Jesse Reichman <14865360+jreichman@users.noreply.github.com>
2025-05-08 12:26:44 +0930

    extract_property parameter - on_none + on_missing
    Change signature for extract_property to define behaviour for on_missing
    and on_none
    Allows None to be supplied to on_missing or on_none via using the
    'Default' class for comparison.

--- src/obslib/__init__.py
+++ src/obslib/__init__.py
@@ -5,6 +5,7 @@ from .obslib import parse_bool
 from .obslib import eval_vars
 from .obslib import Session
 from .obslib import extract_property
+from .obslib import Default
 
 from .exception import *
 
